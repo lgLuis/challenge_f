7b2647ddf8413c5382cb2b3df96a10f299b50cee
lgLuis <luis_unlz@yahoo.com.ar>
2020-12-06 20:11:47 -0300

    casi casi

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,6 +5,96 @@ class Search extends Component {
         super(props);
         this.state = {
             serie: '',
+            image: '#',
+            resumen: '',
+            link: ''
+        }
+    }
+
+
+
+
+
+
+    myAppi = async () => {
+        //        let res = await fetch("http://api.tvmaze.com/search/shows?q=alf")
+        let res = await fetch(`http://api.tvmaze.com/search/shows?q=${this.state.serie}/`)
+        let data = await res.json()
+        console.log(data[0].show.name)
+        this.setState({
+            serie: data[0].show.name,
+            image: data[0].show.image.medium,
+            resumen: data[0].show.summary,
+            link: data[0].show.officialSite
+        })
+    }
+
+    handleName = event => {
+        this.setState({
+            serie: event.target.value
+        })
+    }
+
+    handleSubmit = event =>{
+        var nose = this.state.name
+        console.log(nose)
+        this.myAppi()
+        event.preventDefault()
+    }
+
+ 
+
+    render() {
+        return (
+            <div>
+                <div className="card text-center">
+                    <div className="card-header">
+                        BUSCADOR DE SERIES
+                    </div>
+                    <div className="card-body">
+                        <form onSubmit={this.handleSubmit}>
+                            <label>INGRESE EL NOMBRE DE LA SERIE: </label>
+                            <br />
+                            <input
+                                type="text"
+                                placeholder="por ejemplo ALF"
+                                value={this.state.serie}
+                                onChange={this.handleName}
+                            />
+                            <br /> <br />
+                            <button type="submit" className="btn btn-primary">BUSCAR</button>
+                            <br/>
+
+                            <img src={this.state.image} alt=""/>
+                            <br/>
+                            <a href={this.state.link} target="_blank">SITIO WEB OFICIAL</a>
+                            <p>{this.state.resumen}</p>
+                        </form>
+                    </div>
+                    <div className="card-footer text-muted">
+                        luis 2020
+                    </div>
+                </div>
+
+
+            </div>
+        );
+    }
+}
+
+export default Search;
+
+
+/*
+trae titulo e imagen
+
+import React, { Component } from 'react';
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            serie: '',
             image: '#'
         }
     }
@@ -20,6 +110,7 @@ class Search extends Component {
         let data = await res.json()
         console.log(data[0].show.name)
         this.setState({
+            serie: data[0].show.name,
             image: data[0].show.image.medium
         })
     }
@@ -59,6 +150,7 @@ class Search extends Component {
                             <br /> <br />
                             <button type="submit" className="btn btn-primary">BUSCAR</button>
                             <br />
+                            <p>{this.state.serie}</p>
                             <img src={this.state.image} alt=""/>
                         </form>
                     </div>
@@ -75,84 +167,5 @@ class Search extends Component {
 
 export default Search;
 
-
-
-
-/*
-commit con errores
-import React, { Component } from 'react';
-
-class Search extends Component {
-    constructor(props){
-        super(props);
-        this.state ={
-            serie: '',
-            image: '#'
-        }
-    }
-
-
-
-
-
-
-        myAppi = async ()=>{
-        let res = await fetch(`http://api.tvmaze.com/search/shows?q=${this.state.serie}/`)
-//        let res = await fetch("http://api.tvmaze.com/search/shows?q=alf")
-        let data = await res.json()
-        console.log(data[0].show.name)
-        this.setState({
-            image: data[0].show.image.medium
-        })
-    }
-
-    handleName = event =>{
-        this.setState({
-            serie: event.target.value
-        })
-    }
-
-    handleSubmit = event =>{
-        var nose = this.nose.serie
-        console.log(nose)
-        this.myAppi()
-        event.preventDefault()
-    }
-
-    render() {
-        return (
-            <div>
-                <div className="card text-center">
-                    <div className="card-header">
-                        BUSCADOR DE SERIES
-                    </div>
-                    <div className="card-body">
-                        <form onSubmit={this.handleSubmit}>
-                            <label>INGRESE EL NOMBRE DE LA SERIE: </label>
-                            <br />
-                            <input
-                                type="text"
-                                placeholder="por ejemplo ALF"
-                                value={this.state.serie}
-                                onChange ={this.handleName}
-                            />
-                            <br/> <br/>
-                            <button type="submit" className="btn btn-primary">BUSCAR</button>
-                            <br />
-                            <img src={this.state.image}/>
-                        </form>
-                    </div>
-                    <div className="card-footer text-muted">
-                        luis 2020
-                    </div>
-                </div>
-
-
-            </div>
-        );
-    }
-}
-
-export default Search;
 
 */
