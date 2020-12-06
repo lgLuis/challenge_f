ddc490a1ff92fd1b48f8e3b476c062b5a7aedb94
lgLuis <luis_unlz@yahoo.com.ar>
2020-12-06 16:54:48 -0300

    con errores

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+import React from 'react';
+import Search from './components/Search'
+
+class App extends React.Component{
+  
+    render() {
+      return (
+        <div>
+          <Search/>
+        </div>
+      );
+    }
+}
+
+export default App;
+
+
+
+
+
+/*
+
 import React from 'react'
 import Search from './components/Search'
 
@@ -11,3 +33,5 @@ function App() {
 }
 
 export default App;
+
+*/
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,74 @@
 import React, { Component } from 'react';
 
-class componentName extends Component {
+class Search extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            serie: '',
+            image: '#'
+        }
+    }
+
+
+
+
+
+
+        myAppi = async ()=>{
+        let res = await fetch(`http://api.tvmaze.com/search/shows?q=${this.state.serie}/`)
+//        let res = await fetch("http://api.tvmaze.com/search/shows?q=alf")
+        let data = await res.json()
+        console.log(data[0].show.name)
+        this.setState({
+            image: data[0].show.image.medium
+        })
+    }
+
+    handleName = event =>{
+        this.setState({
+            serie: event.target.value
+        })
+    }
+
+    handleSubmit = event =>{
+        var nose = this.nose.serie
+        console.log(nose)
+        this.myAppi()
+        event.preventDefault()
+    }
+
     render() {
         return (
             <div>
-                <h1>HOLA DESDE EL COMPONENTE</h1>
+                <div className="card text-center">
+                    <div className="card-header">
+                        BUSCADOR DE SERIES
+                    </div>
+                    <div className="card-body">
+                        <form onSubmit={this.handleSubmit}>
+                            <label>INGRESE EL NOMBRE DE LA SERIE: </label>
+                            <br />
+                            <input
+                                type="text"
+                                placeholder="por ejemplo ALF"
+                                value={this.state.serie}
+                                onChange ={this.handleName}
+                            />
+                            <br/> <br/>
+                            <button type="submit" className="btn btn-primary">BUSCAR</button>
+                            <br />
+                            <img src={this.state.image}/>
+                        </form>
+                    </div>
+                    <div className="card-footer text-muted">
+                        luis 2020
+                    </div>
+                </div>
+
                 
             </div>
         );
     }
 }
 
-export default componentName;
+export default Search;
